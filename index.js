d25d0e9318ce30eaf32b8af06585951bd7a398bf
ioan beloane <ioanbeloane@gmail.com>
2023-12-20 13:51:46 +0200

    changed pics v2

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const currentYear = new Date().getFullYear();
 const yearEl = document.querySelector(".year");
 yearEl.textContent = currentYear;
 
-const pic = document.querySelector(".picture-img");
-let n = 1;
-setInterval(function () {
-  pic.src = `/img/hero${n}.webp`;
-  if (n === 4) {
-    n = 1;
-  } else {
-    n++;
-  }
-}, 5 * 1000);
+// const pic = document.querySelector(".picture-img");
+// let n = 1;
+// setInterval(function () {
+//   pic.src = `/img/hero${n}.webp`;
+//   if (n === 4) {
+//     n = 1;
+//   } else {
+//     n++;
+//   }
+// }, 5 * 1000);
